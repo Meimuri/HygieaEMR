af78fe49f4f9e127eb8591b183ce6ef1524cbb0e
Michael A. Alcaraz II <22783799+Meimuri@users.noreply.github.com>
2024-01-13 06:00:43 +0800

    Update GET /api/patient/:id to fetch address

--- server/src/utils/middleware/patient/patient_finder.js
+++ server/src/utils/middleware/patient/patient_finder.js
@@ -1,8 +1,20 @@
 // Internal modules
-const { Patient } = require("../../../models");
+const { Patient, PatientAddress } = require("../../../models");
 
 const patientFinder = async (req, res, next) => {
-    const patient = await Patient.findByPk(req.params.id);
+    // const patient = await Patient.findByPk(req.params.id);
+
+    const patient = await Patient.findByPk(req.params.id, {
+        attributes: { exclude: ["createdAt", "updatedAt"] },
+        include: [
+            {
+                model: PatientAddress,
+                attributes: {
+                    exclude: ["createdAt", "updatedAt", "patientId"],
+                },
+            },
+        ],
+    });
 
     if (!patient) {
         return res.status(404).json({ error: "Patient not found" });
