e2f2f3085093024f1438a830ae42a40dfd075bb5
Michael A. Alcaraz II <22783799+Meimuri@users.noreply.github.com>
2024-01-21 09:28:29 +0800

    Rename folder to follow case sensitivity rules

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import Notification from "./common/components/Notification";
-import LoginForm from "./modules/Login/components/LoginForm";
+import LoginForm from "./features/login/components/LoginForm";
 
 const App = () => {
     return (
