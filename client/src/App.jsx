cad0a179c05004df6dcc17716375f22cbcee55fc
Michael A. Alcaraz II <22783799+Meimuri@users.noreply.github.com>
2024-02-10 05:44:20 +0800

    Remove extra component loading when checking logged users

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import Main from "./features/main/components/Main";
 
 const App = () => {
     const dispatch = useDispatch();
-    const { user, isLoading } = useSelector((state) => state.login);
+    const { user } = useSelector((state) => state.login);
 
     useEffect(() => {
         dispatch(handleLocalStorageLogin());
@@ -18,13 +18,7 @@ const App = () => {
     return (
         <div>
             <Notification />
-            {isLoading ? (
-                <div>Loading...</div>
-            ) : !user ? (
-                <LoginForm />
-            ) : (
-                <Main />
-            )}
+            {!user ? <LoginForm /> : <Main />}
         </div>
     );
 };
